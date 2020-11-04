1e03ed967dee8387888adb6f4487dfc98efda902
Guoker3 <47735844+Guoker3@users.noreply.github.com>
2020-11-04 17:57:39 +0800

    diffusion of the step mode

---
+++ loadData.py
@@ -0,0 +1,93 @@
+from csv import reader as csvReader
+import numpy as np
+import pandas as pd
+
+
+class d_apyori_cookDataSet:
+    def __init__(self):
+        """
+        :param
+            header: header list of the dataset if there is header in.
+            r_data: data set in form of list<float>
+
+            n_data: normalized data set
+            n_data_inf: information about the normalization
+
+            d_data: divided data set to make float data from different attribute not mixed in the algorithm
+                    [0.1,0.4,0.2] -> [0.1, 3.4, 6.2] when divideStep == 3
+        """
+        self.fileName = None
+        self.header = None
+        self.r_data = None
+
+        self.n_data = None
+        self.n_data_inf = dict()
+
+        self.d_data = None
+
+    def __loadCSV(self, csvName, haveHeader, myEncoding='utf-8'):
+        data = []
+        with open(csvName, encoding=myEncoding) as csvFile:
+            csv_reader = csvReader(csvFile)  # 使用csv.reader读取csvFile中的文件
+            if haveHeader:
+                self.header = next(csv_reader)
+                # print(csvName, " header : ", header)
+            for row in csv_reader:
+                data.append(row)
+        self.r_data = [[float(x) for x in row] for row in data]
+        print("csv loaded")
+
+    def loadDataSet(self, fileName, haveHeader, myEncoding='utf-8'):
+        self.fileName = fileName
+        if fileName[-4:] == '.csv':
+            self.__loadCSV(fileName, haveHeader=haveHeader, myEncoding=myEncoding)
+        else:
+            raise TypeError('unknown kind of files')
+
+    def normalization(self):
+        if self.r_data == None:
+            raise Exception('raw data have not loaded')
+        if self.header == None:
+            self.header = [x for x in range(len(self.r_data[0]))]
+        pd_r_data = pd.DataFrame(self.r_data, columns=self.header)
+        n_inf_min = []
+        n_inf_range = []
+        for i in range(len(self.r_data[0])):
+            minData = np.min(pd_r_data[self.header[i]].tolist())
+            n_inf_min.append(minData)
+            t_range = np.max(pd_r_data[self.header[i]].to_list()) - minData
+            n_inf_range.append(t_range)
+
+            def normalization(number):
+                return (number - minData) / t_range
+
+            pd_r_data[self.header[i]] = pd.Series([normalization(x) for x in pd_r_data[self.header[i]].tolist()])
+        self.n_data_inf['min'] = n_inf_min
+        self.n_data_inf['range'] = n_inf_range
+        self.n_data = pd_r_data.values.tolist()
+        print('data normalized')
+
+    def division(self):
+        if self.n_data == None:
+            raise Exception('have no data set normalized')
+        if self.header == None:
+            self.header = [x for x in range(len(self.r_data[0]))]
+        divideStep = 3
+        pd_n_data = pd.DataFrame(self.r_data, columns=self.header)
+        for i in range(len(self.r_data[0])):
+            pd_n_data[self.header[i]] = pd.Series([x + divideStep * i for x in pd_n_data[self.header[i]].tolist()])
+        self.d_data = pd_n_data.values.tolist()
+        print('data divided')
+
+    def quickStart(self, fileName='test9_11.csv', haveHeader=True):
+        self.loadDataSet(fileName=fileName, haveHeader=haveHeader)
+        self.normalization()
+        self.division()
+        print('quick started')
+
+
+if __name__ == '__main__':
+    t = d_apyori_cookDataSet()
+    t.loadDataSet('test9_11.csv', haveHeader=True)
+    t.normalization()
+    t.division()
---
+++ step_mode.py
@@ -0,0 +1,40 @@
+def stepDiffusion(d_dataSet,diffusion_t,diffusion_d,mode='weight'):
+    """
+    :param
+        d_dataSet:  divided dataset(also normalized).
+
+        diffusion_t:    times of dissusion          ,like [1,3,5,2,1]
+        diffusion_d:    each attributes' diffusion distance,like [[-0.2,-0.1,0,0.1,0.2], ...]
+        # len(diffusion_d[i])=len(diffusion_t)
+
+    :mode param:
+        mode =='weight': transfer parameter of the weight into the algorithm for calculate.
+        mode == 'brute':    repeat the record in dataSet according to the weight<int>.
+    """
+    ret_dataSet=[]
+    if mode=='brute':
+        for record in d_dataSet:
+            for dd in range(len(diffusion_t)):
+                record_t=[x+y[dd] for x in record for y in diffusion_d ]
+                for t in range(diffusion_t[dd]):
+                    ret_dataSet.append(record_t)
+        return ret_dataSet
+
+    if mode=='weight':
+        for record in d_dataSet:
+            for dd in range(len(diffusion_t)):
+                record_t = [x + y[dd] for x in record for y in diffusion_d]
+                ret_dataSet.append([record_t,diffusion_t[dd]])
+        return ret_dataSet
+
+if __name__=='__main__':
+    from loadData import *
+    dataSet = d_apyori_cookDataSet()
+    dataSet.quickStart(fileName='test9_11.csv', haveHeader=True)
+    ret_dataSet_brute=stepDiffusion(dataSet.n_data,[1,3,1],[[-0.1,0,0.1],]*len(dataSet.n_data[0]),mode='brute')
+    #for i in ret_dataSet_brute:
+    #    print(i)
+
+    ret_dataSet_weight=stepDiffusion(dataSet.n_data,[1,3,1],[[-0.1,0,0.1],]*len(dataSet.n_data[0]),mode='weight')
+    for i in ret_dataSet_weight:
+        print(i)
