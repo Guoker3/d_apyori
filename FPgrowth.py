5492b9d61013f2b75d80e8dc639b9480ff482e89
Guoker3 <47735844+Guoker3@users.noreply.github.com>
2020-11-16 17:34:59 +0800

    support_data correct waiting

--- FPgrowth.py
+++ FPgrowth.py
@@ -18,14 +18,12 @@ class Fp_growth():
         if mode == 'brute':
             self.generate_R = self.generate_R_brute
             self.generate_L = self.generate_L_brute
-            self.find_cond_pattern_base = self.find_cond_pattern_base_brute
             self.create_cond_fptree = self.create_cond_fptree_brute
             self.create_fptree = self.create_fptree_brute
             self.update_fptree = self.update_fptree_brute
         if mode == 'weight':
             self.generate_R = self.generate_R_weight
             self.generate_L = self.generate_L_weight
-            self.find_cond_pattern_base = self.find_cond_pattern_base_weight
             self.create_cond_fptree = self.create_cond_fptree_weight
             self.create_fptree = self.create_fptree_weight
             self.update_fptree = self.update_fptree_weight
@@ -50,6 +48,20 @@ class Fp_growth():
         for r in range(len(rl)):
             rl[r].append(supportData[rl[r][0]])
         return rl
+
+    def find_cond_pattern_base(self, node_name, headerTable):
+        '''
+        根据节点名字，找出所有条件模式基
+        '''
+        treeNode = headerTable[node_name][1]
+        cond_pat_base = {}  # 保存所有条件模式基
+        while treeNode != None:
+            nodepath = []
+            self.find_path(treeNode, nodepath)
+            if len(nodepath) > 1:
+                cond_pat_base[frozenset(nodepath[:-1])] = treeNode.count
+            treeNode = treeNode.nodeLink
+        return cond_pat_base
 
 #************************************part of BRUTE**********************************************************
     def update_fptree_brute(self, items, node, headerTable):  # 用于更新fptree
@@ -104,20 +116,6 @@ class Fp_growth():
                 # 用过滤且排序后的样本更新树
                 self.update_fptree(order_item, tree_header, headerTable)
         return tree_header, headerTable
-
-    def find_cond_pattern_base_brute(self, node_name, headerTable):
-        '''
-        根据节点名字，找出所有条件模式基
-        '''
-        treeNode = headerTable[node_name][1]
-        cond_pat_base = {}  # 保存所有条件模式基
-        while treeNode != None:
-            nodepath = []
-            self.find_path(treeNode, nodepath)
-            if len(nodepath) > 1:
-                cond_pat_base[frozenset(nodepath[:-1])] = treeNode.count
-            treeNode = treeNode.nodeLink
-        return cond_pat_base
 
     def create_cond_fptree_brute(self, headerTable, min_support, temp, freq_items, support_data):
         # 最开始的频繁项集是headerTable中的各元素
@@ -253,81 +251,30 @@ class Fp_growth():
                 self.update_fptree_weight(order_item, tree_header, headerTable,t[1])
         return tree_header, headerTable
 
-    def find_cond_pattern_base_weight(self, node_name, headerTable):
-        '''
-        根据节点名字，找出所有条件模式基
-        '''
-        if type(list(node_name)[0])==type(frozenset()):
-            tmp=list(node_name)[0]
-        else:
-            tmp=list(node_name)[1]
-        treeNode = headerTable[tmp][1]
-        cond_pat_base = {}  # 保存所有条件模式基
-        while treeNode != None:
-            nodepath = []
-            self.find_path(treeNode, nodepath)
-            if len(nodepath) > 1:
-                nodepath_tmp=[]
-                for i in nodepath[:-1]:
-                    ii=list(i)
-                    if type(ii[0])==type(frozenset()):
-                        nodepath_tmp.append(ii[0])
-                    else:
-                        nodepath_tmp.append(ii[1])
-
-                cond_pat_base[nodepath_tmp] = treeNode.count
-            treeNode = treeNode.nodeLink
-        #print('cpd:',cond_pat_base)
-        return cond_pat_base
-
     def create_cond_fptree_weight(self, headerTable, min_support, temp, freq_items, support_data):
-        """:param
-                headerTable:dictionary of the "('39.6', [18, <__main__.Node object at 0x000001B355482DC8>])"
-        """
         # 最开始的频繁项集是headerTable中的各元素
-        freqs = [v for v in sorted(headerTable.items(), key=lambda p: p[1][0])]  # 根据频繁项的总频次排序
-        for i in range(len(freqs)):
-            freqs[i]=frozenset([freqs[i][0],freqs[i][1][0]])
-
+        freqs = [v[0] for v in sorted(headerTable.items(), key=lambda p: p[1][0])]  # 根据频繁项的总频次排序
         for freq in freqs:  # 对每个频繁项
             freq_set = temp.copy()
             freq_set.add(freq)
-
             freq_items.add(frozenset(freq_set))
-            ##TODO(main) rewrite the frozenset to make it ordered and repeat-able OR make list to str for hash
-
-            if type(list(freq)[0]) == type(frozenset()):
-                tmp = list(freq)[0]
-            else:
-                tmp = list(freq)[1]
-            ##TODO(doing) make weight added rather than support
             if frozenset(freq_set) not in support_data:  # 检查该频繁项是否在support_data中
-                #print(freq_set)
-                support_data[frozenset(freq_set)] = headerTable[tmp][0]
-                #print(support_data[frozenset(freq_set)])
+                support_data[frozenset(freq_set)] = headerTable[freq][0]
             else:
-                #print(freq_set)
-                support_data[freq_set] += headerTable[tmp][0]
-                #print(support_data[frozenset(freq_set)])
+                support_data[frozenset(freq_set)] += headerTable[freq][0]
 
-            cond_pat_base = self.find_cond_pattern_base(freq, headerTable)  # 寻找到所有条件模式基 ##TODO(done) make support return together
-            #for i in cond_pat_base:
-            #    print(i)
+            cond_pat_base = self.find_cond_pattern_base(freq, headerTable)  # 寻找到所有条件模式基
             cond_pat_dataset = []  # 将条件模式基字典转化为数组
-            for item in cond_pat_base.items():
-                item_temp = list(item[0])
-                #print('item_temp:',item_temp)
+            for item,weight in cond_pat_base.items():
+                item_temp = list(item)
                 item_temp.sort()
-                #print(item)
-                cond_pat_dataset.append([item_temp,item[1]])
-            #for i in cond_pat_dataset:
-                #print(':::',i)
+                for i in range(cond_pat_base[item]):
+                    cond_pat_dataset.append([item_temp,weight])
             # 创建条件模式树
-            cond_tree, cur_headtable = self.create_fptree_weight(cond_pat_dataset, min_support)
+            cond_tree, cur_headtable = self.create_fptree(cond_pat_dataset, min_support)
             if cur_headtable != None:
-                #print(cur_headtable)
-                #input()
-                self.create_cond_fptree_weight(cur_headtable, min_support, freq_set, freq_items, support_data)  # 递归挖掘条件FP树
+                self.create_cond_fptree_weight(cur_headtable, min_support, freq_set, freq_items,
+                                              support_data)  # 递归挖掘条件FP树
 
     def generate_L_weight(self, data_set, min_support):
         freqItemSet = set()
@@ -336,7 +283,7 @@ class Fp_growth():
         #print('T:',tree_header)
         #print('H:',headerTable)
         # 创建各频繁一项的fptree，并挖掘频繁项并保存支持度计数
-        self.create_cond_fptree_weight(headerTable, min_support, set(), freqItemSet, support_data)
+        self.create_cond_fptree(headerTable, min_support, set(), freqItemSet, support_data)
         max_l = 0
         for i in freqItemSet:  # 将频繁项根据大小保存到指定的容器L中
             if len(i) > max_l: max_l = len(i)
@@ -429,7 +376,8 @@ if __name__ == "__main__":
         new_min_support = int(min_support_param * sum(dif_list))
 
     if dataChoice=='small':
-        easyDataSet=[[0,0],[1,1],[0,0]]
+        easyDataSet=[[0,0,0],[1,1,1],[0,0,1]]
+        #easyDataSet=[[0,0,0],[1,1,1],[0,0,0.2],[0,0,0.7],[0.1,0.2,0.3]]
         dataSet.quickStart(fileName=easyDataSet,haveHeader=False)
 
         #dif_list=[1,3,1]
@@ -438,9 +386,9 @@ if __name__ == "__main__":
         dif_step=[[0,], ] * len(dataSet.n_data[0])
 
         new_min_support = 1
-
-    mode = 'weight'
-    #mode = 'brute'
+    mode = None
+    #mode = 'weight'
+    mode = 'brute'
 
     print('****************\n******************* brute ******************\n***************************')
     if mode == 'brute':
